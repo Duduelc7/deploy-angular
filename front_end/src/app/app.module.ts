ce37c448633842cd637ccd3f36a8e9f2f408297b
Felipe Zampa <86922370+felipezampa@users.noreply.github.com>
2023-04-13 16:14:13 -0300

    retangulo verde improvisado no mobile

--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -1,12 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxMaskModule } from 'ngx-mask';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ThankYouComponent } from './thank-you/thank-you.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxMaskModule,} from 'ngx-mask';
 
 @NgModule({
   declarations: [
