0167fabfcda8a2d8aec3495d0e090b98766b2d94
Felipe Zampa <86922370+felipezampa@users.noreply.github.com>
2023-04-17 14:37:24 -0300

    texto novo e mais opcoes no select

--- front_end/src/app/landing-page/landing-page.component.ts
+++ front_end/src/app/landing-page/landing-page.component.ts
@@ -13,9 +13,9 @@ export class LandingPageComponent implements OnInit {
 
   @ViewChild('formContato') formContato!: NgForm;
   @ViewChild('name', { static: true }) inputFocus!: ElementRef;
-  SegmentoChoices: String[] = ['Comércio', 'Indústria', 'Serviços'];
+  SegmentoChoices: String[] = ['Comércio', 'Indústria', 'Serviços','Agricultura','Pecuária','Outros'];
   FaturamentoChoices: String[] = ['Ainda não faturamos', 'Até R$500 mil ao ano', 'De R$500 mil a R$1 milhão ao ano', 'De R$1 milhão a R$5 milhões ao ano', 'De R$5 a R$10 milhões ao ano', 'De R$10 a R$50 milhões ao ano', 'De R$50 a R$500 milhões ao ano', 'Acima de R$500 milhões'];
-  CargoChoices: String[] = ['Analista', 'Supervisor', 'Coordenador', 'Gerente', 'Diretor', 'Vice-presidente ou C-Level', 'Presidente ou CEO', 'Sócio ou Fundador'];
+  CargoChoices: String[] = ['Analista', 'Supervisor', 'Coordenador', 'Gerente', 'Diretor', 'Vice-presidente ou C-Level', 'Presidente ou CEO', 'Sócio ou Fundador','Proprietário'];
 
   constructor(private router: Router, private emailService: EmailService) { }
 
@@ -30,6 +30,4 @@ export class LandingPageComponent implements OnInit {
     var meuInput = document.getElementById("inputFocus");
     meuInput?.focus();
   }
-  // privacidade(){
-  // }
 }
